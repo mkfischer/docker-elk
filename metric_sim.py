4fe501c87ff991958c9165b19e18a3915c2f4018
Mike Fischer <mkfischer1970@icloud.com>
2023-12-01 12:45:46 +0100

    better metric sim

--- metric_sim.py
+++ metric_sim.py
@@ -2,6 +2,13 @@ from flask import Flask, request, jsonify, render_template_string
 from prometheus_flask_exporter import PrometheusMetrics
 import random
 import time
+import logging
+import logstash
+
+# Configure logging to send logs to Logstash
+logger = logging.getLogger('my_logger')
+logger.setLevel(logging.INFO)
+logger.addHandler(logstash.LogstashHandler('localhost', 5000))
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
@@ -11,13 +18,30 @@ cpu_usage = metrics.gauge('cpu_usage', 'CPU usage')
 disk_usage = metrics.gauge('disk_usage', 'Disk usage')
 ram_usage = metrics.gauge('ram_usage', 'RAM usage')
 
+initial_disk = random.randint(10, 40)
+initial_cpu = random.randint(20, 35)
+initial_ram = random.randint(30, 50)
+
 # Define the metrics values dictionary
 metrics_values = {
-    'cpu_usage': 0,
-    'disk_usage': 0,
-    'ram_usage': 0,
+    'cpu_usage': initial_cpu,
+    'disk_usage': initial_disk,
+    'ram_usage': initial_ram,
     'running': False
 }
+
+# Define the log entry templates
+log_entry_templates = [
+    "CPU load is high: {{ cpu_usage }}%",
+    "Disk usage is high: {{ disk_usage }}%",
+    "RAM usage is high: {{ ram_usage }}%",
+    "Critical error detected!"
+]
+
+
+def log_entry(log_message):
+    # Generate a log entry with the provided message
+    logger.info(log_message)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -48,13 +72,22 @@ def index():
             metrics_values['disk_usage'] = random.randint(80, 100)
             metrics_values['ram_usage'] = random.randint(80, 100)
             metrics_values['running'] = True
+
+            # Log the initial state of the application
+            log_entry("Application started with CPU: {}%, Disk: {}%, RAM: {}%".format(
+                metrics_values['cpu_usage'], metrics_values['disk_usage'], metrics_values['ram_usage']))
+
             return render_template_string(html_content, status='Running')
         elif action == 'end':
-            # Decrease metrics to 0
-            metrics_values['cpu_usage'] = 0
-            metrics_values['disk_usage'] = 0
-            metrics_values['ram_usage'] = 0
+            # Decrease metrics to default values
+            metrics_values['cpu_usage'] = initial_cpu
+            metrics_values['disk_usage'] = initial_disk
+            metrics_values['ram_usage'] = initial_ram
             metrics_values['running'] = False
+
+            # Log the end of the test
+            log_entry("Application test ended. Metrics reset to default values.")
+
             return render_template_string(html_content, status='Stopped')
 
     return render_template_string(html_content, status='')
